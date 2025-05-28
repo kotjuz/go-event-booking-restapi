593d302d91dc9bba21a38d34b1b077fddf80e251
kotjuz <rkotarski10@gmail.com>
2025-05-28 17:54:18 +0200

    feat: added DELETE endpoint to delete event

--- routes/events.go
+++ routes/events.go
@@ -89,3 +89,23 @@ func updateEvent(context *gin.Context) {
 	}
 	context.JSON(http.StatusOK, gin.H{"message": "Updated event"})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Wrong id value"})
+	}
+
+	event, err := models.GetSingleEvent(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get event from database"})
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete event from database"})
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Deleted event"})
+}
