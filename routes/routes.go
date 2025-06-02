22fe38a132e50273c66348d33e13da63d1a9310e
kotjuz <rkotarski10@gmail.com>
2025-06-02 20:44:32 +0200

    feat: added registering user for event logic

--- routes/routes.go
+++ routes/routes.go
@@ -14,6 +14,8 @@ func RegisterRoutes(server *gin.Engine) {
 	authenticated.POST("/events", createEvent)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
+	authenticated.POST("/events/:id/register", registerForEvent)
+	authenticated.DELETE("/events/:id/register", deleteEvent)
 
 	server.POST("/signup", createUser)
 	server.POST("/login", loginUser)
